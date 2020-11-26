7f13df423da20069eb06e064012bc62e13815822
Kirill Vasiliev <vasiliev.k.v@gmail.com>
2020-11-26 23:45:26 +0300

    MVI View Model

--- app/src/main/java/ru/vasiliev/architecturesandbox/core/mvi/MviViewModel.kt
+++ app/src/main/java/ru/vasiliev/architecturesandbox/core/mvi/MviViewModel.kt
@@ -4,5 +4,5 @@ import io.reactivex.rxjava3.core.Observable
 
 interface MviViewModel<I : MviIntent, S : MviViewState> {
     fun process(intents: Observable<I>)
-    fun render(viewState: S)
+    fun states(viewState: S)
 }
